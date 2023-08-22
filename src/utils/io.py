2d39bc00a17004ef8c2dc48b62005a62cc125209
ZhrA <zaaahra.amini@gmail.com>
2023-08-22 14:13:48 +0330

    Add JSON file I/O module to utils package

---
+++ src/utils/io.py
@@ -0,0 +1,40 @@
+import json
+from pathlib import Path
+
+OUPUT_DIR = Path(__file__).resolve().parent.parent
+
+
+def read_json(file_path: str) -> dict:
+
+    """Reads and parses data from a JSON file.
+
+    Raises:
+        FileNotFoundError: If the specified file does not exist.
+        json.JSONDecodeError: If the JSON data cannot be decoded.
+    """
+    try:
+        with open(file_path, 'r') as json_file:
+            data = json.load(json_file)
+            return data
+    except FileNotFoundError:
+        raise FileNotFoundError(f"File '{file_path}' not found.")
+    except json.JSONDecodeError as e:
+        raise json.JSONDecodeError(f"Error decoding JSON in '{file_path}': {e}", e.doc, e.pos)
+
+
+def write_file(file_path: str, file_name: str, words_list: list,
+               output_dir=OUPUT_DIR/'result_Data'):
+
+    """Reads the contents of a text file.
+
+    Raises:
+        FileNotFoundError: If the specified file does not exist.
+    """
+
+    try:
+        with open(str(output_dir / file_name), 'w') as f:
+            list = words_list
+            formatted_list = {f"{item}\n" for item in list}
+            f.writelines(formatted_list)
+    except FileNotFoundError:
+        raise FileNotFoundError(f"File '{file_path}' not found.")
